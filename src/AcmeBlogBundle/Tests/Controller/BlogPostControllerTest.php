f9066822cf45fab7d9ba571132316b337777e18e
Alexandr Kalenyuk <kalenyuk.alex@gmail.com>
2015-03-30 16:02:27 +0300

    add testShowPost

--- src/AcmeBlogBundle/Tests/Controller/BlogPostControllerTest.php
+++ src/AcmeBlogBundle/Tests/Controller/BlogPostControllerTest.php
@@ -27,9 +27,9 @@ class BlogPostControllerTest extends WebTestCase
             $crawler->filter('a:contains("Login")')->count()
         );
 
-        $buttonCrawlerNode = $crawler->selectButton('login');
+        $buttonCrawlerNode = $crawler->selectButton('Login');
         $form = $buttonCrawlerNode->form([
-            "_username" => 'email1@mail.com',
+            "_username" => 'email2@mail.com',
             "_password" => 'qwezxc',
         ]);
         $this->client->submit($form);
@@ -42,6 +42,29 @@ class BlogPostControllerTest extends WebTestCase
         $this->assertEquals(
             1,
             $crawler->filter('a:contains("Logout")')->count()
+        );
+    }
+
+    public function testShowPost()
+    {
+        $this->client->request('GET', '/blog/');
+        $crawler = $this->client->followRedirect();
+        $buttonCrawlerNode = $crawler->selectButton('Login');
+        $form = $buttonCrawlerNode->form([
+            "_username" => 'email2@mail.com',
+            "_password" => 'qwezxc',
+        ]);
+        $this->client->submit($form);
+        $crawler = $this->client->followRedirect();
+
+        $titleEl = $crawler->filter('h3 a')->eq(1);
+        $titleText = $titleEl->text();
+        $titleLink = $titleEl->link();
+        $this->client->click($titleLink);
+
+        $this->assertEquals(
+            1,
+            $crawler->filter('h3:contains("'.$titleText.'")')->count()
         );
     }
 }
